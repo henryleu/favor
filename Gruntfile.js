ac718fe041f2b88bd772bba15dabbc7590bd61db
henryleu <henryleu@126.com>
2013-07-28 22:13:12 +0800

    use requirejs manage assets and switch development and production

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,127 +9,133 @@ function createFileSha(filenane) {
 module.exports = function(grunt) {
 	grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        copy: {
-            components: {
-                files: [
-                    {
-                        expand: true, flatten: false,
-                        cwd: 'public/components/bootstrap-tl/tl-bootstrap/img/',
-                        src: ['**'],
-                        dest: 'public/components/tl/img/'
-                    },
-                    {
-                        expand: true, flatten: false, filter: 'isFile',
-                        cwd: 'public/components/font-awesome/font/',
-                        src: ['**'],
-                        dest: 'public/components/tl/font/'
-                    }
-                ]
-            }
-        },
-        concat: {
-            components_css: {
-                options: {
-                    banner: '/*! all third-party style-sheets including bootstrap and font-awesome/\n'
-                },
-                src: [
-                    'public/components/bootstrap-tl/tl-bootstrap/css/tl-bootstrap.css',
-                    'public/components/bootstrap-tl/tl-bootstrap/css/tl-bootstrap-responsive.css',
-                    'public/components/font-awesome/css/font-awesome.css'
-                ],
-                dest: 'public/components/tl/css/dependencies.css'
-            },
-
-            components_js: {
-                options: {
-                    banner: '/*! all third-party scripts including jquery, underscore, backbone and bootstrap */\n'
-                },
-                src: [
-                    'public/components/jquery/jquery.js',
-                    'public/components/jquery.cookie/jquery.cookie.js',
-                    'public/components/underscore/underscore.js',
-                    'public/components/backbone/backbone.js',
-                    'public/components/bootstrap-tl/tl-bootstrap/js/tl-bootstrap.js'
-                ],
-                dest: 'public/components/tl/js/dependencies.js'
-            },
-            tl_css: {
-                options: {
-                    banner: '/*! Tomato Labs webapp main css */\n'
-                },
-                src: [
-                    'public/tl/css/favor.css'
-                ],
-                dest: 'public/tl/css/main.css'
-            },
-            tl_js: {
-                options: {
-                    banner: '/*! Tomato Labs webapp main js */\n'
-                },
-                src: [
-                    'public/tl/js/tomato-spa.js',
-                    'public/tl/js/favor.js'
-                ],
-                dest: 'public/tl/js/main.js'
-            }
-        },
-        recess: {
-            dist: {
-                options: {
-                    compress: true
-                },
-                files: [
-                    {'public/components/tl/css/dependencies.min.css': ['public/components/tl/css/dependencies.css']},
-                    {'public/tl/css/main.min.css': ['public/tl/css/main.css']}
-                ]
-            }
-        },
         jst: {
             compile: {
                 options: {
                     processName: function(filename) {
-                        return filename.substring('templates/'.length, filename.indexOf('.htm'));
+                        return filename.substring('web/template/'.length, filename.indexOf('.html'));
                     },
                     templateSettings: {
                         interpolate : /<%=([\s\S]+?)%>/g
                     }
                 },
                 files: {
-                    "public/tl/js/templates.js": ["templates/*.html"]
+                    "public/build/js/templates.js": ["web/template/*.html"]
                 }
             }
         },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd hh:MM:ss") %> */\n'
+        sprite: {
+            all: {
+                // List of images to add to sprite
+                'src': [ 'web/img/*.png' ],
+                // Address of target image
+                'destImg': 'public/build/img/sprite.png',
+                'destCSS':  'web/css/sprite.less',
+                'imgPath': '../sprite.png',
+                // OPTIONAL: Image placing algorithm: top-down, left-right, diagonal, alt-diagonal
+                'algorithm': 'alt-diagonal',
+                // OPTIONAL: Specify padding between images
+                'padding': 2,
+                // OPTIONAL: Rendering engine: auto, canvas, gm
+                'engine': 'gm',
+                // OPTIONAL: Specify CSS format (inferred from destCSS' extension by default)
+                // (stylus, scss, sass, less, json, css)
+                'cssFormat': 'json',
+                // OPTIONAL: Preferences for resulting image
+                'exportOpts': {
+                    'imagemagick': true,
+                    // Image formst (buy default will try to use dest extension)
+                    'format': 'png',
+                    // Quality of image (gm only)
+                    'quality': 90
+                },
+                // OPTIONAL: Specify img options
+                'imgOpts': {
+                    // Format of the image (inferred from destImg' extension by default) (jpg, png)
+                    'format': 'png',
+
+                    // Quality of image (gm only)
+                    'quality': 90
+                },
+                // OPTIONAL: Specify css options
+                'cssOpts': {
+                    // Some templates allow for skipping of function declarations
+                    'functions': false
+                }
+            }
+        },
+        requirejs: {
+            js: {
+                options: {
+                    baseUrl: "web/js",
+                    mainConfigFile: "web/js/main.js",
+                    include: ['requireLib'],
+                    name: 'main',
+                    out: "public/build/main.js"
+                }
             },
-            components: {
-                src: 'public/components/tl/js/dependencies.js',
-                dest: 'public/components/tl/js/dependencies.min.js'
+            css: {
+                options: {
+                    baseUrl: 'web/css',
+                    cssIn: "web/css/main.css",
+                    out: "public/build/main.css",
+                    cssImportIgnore: null,
+                    optimizeCss: 'default'
+                }
+            }
+        },
+        watch: {
+            img: {
+                files: ['web/img/*'],
+                tasks: ['requirejs:css'],
+                options: {
+                    nospawn: true,
+                    interrupt: false
+                }
+            },
+            css: {
+                files: ['web/css/*.css'],
+                tasks: ['requirejs:css'],
+                options: {
+                    nospawn: true,
+                    interrupt: false
+                }
             },
             templates: {
-                src: 'public/tl/js/templates.js',
-                dest: 'public/tl/js/templates.min.js'
+                files: ['web/template/*.html'],
+                tasks: ['jst'],
+                options: {
+                    nospawn: true,
+                    interrupt: false
+                }
             },
-            main: {
-                src: 'public/tl/js/main.js',
-                dest: 'public/tl/js/main.min.js'
+            js: {
+                files: ['web/js/*.js'],
+                tasks: ['requirejs:js'],
+                options: {
+                    nospawn: true,
+                    interrupt: false
+                }
             }
         }
 	});
 
 	// Laoded tasks
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+//    grunt.loadNpmTasks('grunt-contrib-copy');
+//    grunt.loadNpmTasks('grunt-contrib-concat');
+//    grunt.loadNpmTasks('grunt-contrib-uglify');
+//    grunt.loadNpmTasks('grunt-recess');
+//	  grunt.loadNpmTasks('grunt-hashres');
+//    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-jst');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-recess');
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    //grunt.loadNpmTasks('grunt-contrib-jshint');
-	//grunt.loadNpmTasks('grunt-contrib-requirejs');
-	//grunt.loadNpmTasks('grunt-hashres');
 
 	// Default task.
 	//grunt.registerTask('default', ['jshint', 'requirejs', 'hashres']);
-    grunt.registerTask('default', ['copy', 'concat', 'recess', 'jst', 'uglify']);
+//    grunt.registerTask('default', ['copy',  'jst', 'concat', 'recess','uglify']);
+    grunt.registerTask('default', ['jst', 'requirejs']);
+    grunt.registerTask('wat', ['watch']);
 };
