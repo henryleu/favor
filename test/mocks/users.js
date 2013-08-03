0b6a867235c2fa29e401a4d3b4e8780240a5e171
henryleu <henryleu@126.com>
2013-08-03 22:15:29 +0800

    Add user test and mocks

---
+++ test/mocks/users.js
@@ -0,0 +1,11 @@
+module.exports = [{
+    "id": 0,
+    "_mv": 0, //document model version: the structure's version
+    "_dv": 0, //document version: the record's version
+    'username': 'henryleu'
+},{
+    "id": 1,
+    "_mv": 0, //document model version: the structure's version
+    "_dv": 0, //document version: the record's version
+    'username': 'riversyang'
+}];
