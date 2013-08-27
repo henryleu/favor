2c7e492db8aedaa02e7789f32e037b7591fb9622
riversyang <rivers.yang@gmail.com>
2013-08-28 01:41:14 +0800

    Try to change the implementation of google analytics setup for backbone.

--- web/js/main.js
+++ web/js/main.js
@@ -20,14 +20,11 @@ require.config({
         'Fileupload': {
             deps: ['jQuery', 'jquery.ui.widget']
         },
-        'backbone.analytics': {
-            deps: ['Backbone', 'Underscore']
-        },
         'JST': {
             exports: 'JST'
         },
         'Util': {
-            deps: ['jQuery']
+            deps: ['jQuery', 'Underscore', 'Backbone']
         }
     },
     paths: {
@@ -38,14 +35,13 @@ require.config({
         Bootstrap: '../../public/components/bootstrap-tl/tl/js/bootstrap',
         'jquery.ui.widget':  '../../public/components/jquery-file-upload/js/vendor/jquery.ui.widget',
         Fileupload: '../../public/components/jquery-file-upload/js/jquery.fileupload',
-        'backbone.analytics': '../../public/components/backbone.analytics/backbone.analytics',
         JST: '../../public/build/js/templates',
         Spa: 'backbone-spa',
         Favor: 'favor',
         Util: 'util',
         App: 'app'
     },
-    deps: ['App', 'Util', 'backbone.analytics'],
+    deps: ['App', 'Util'],
     callback: function(){
     },
     preserveLicenseComments: false
--- web/js/util.js
+++ web/js/util.js
@@ -78,3 +78,31 @@ Date.prototype.toString=function(format,loc){
     }
     return format;
 };
+
+(function() {
+    var loadUrl = Backbone.History.prototype.loadUrl;
+
+    Backbone.History.prototype.loadUrl = function(fragmentOverride) {
+        var matched = loadUrl.apply(this, arguments),
+            gaFragment = this.fragment;
+        if (!/^\//.test(gaFragment)) gaFragment = '/' + gaFragment;
+        ga('send', 'pageview');
+        console.log('send pageview - ' + gaFragment);
+        if(window._gaq !== undefined) {
+            window._gaq.push(['_trackPageview', gaFragment]);
+            ga('send', 'pageview');
+            console.log('send pageview - ' + gaFragment);
+        }
+        return matched;
+    };
+
+}).call(this);
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-43530384-1', 'realsaas.com');
+ga('send', 'pageview');
+console.log('send pageview - init');
