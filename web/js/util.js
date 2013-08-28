2bfb437dc23a2fd42fde9a958016aabb29438eb2
riversyang <rivers.yang@gmail.com>
2013-08-28 13:17:29 +0800

    Merge branch 'master' into riversyang

--- web/js/util.js
+++ web/js/util.js
@@ -78,3 +78,12 @@ Date.prototype.toString=function(format,loc){
     }
     return format;
 };
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-43530384-1', 'realsaas.com');
+//ga('send', 'pageview');
+console.log('begin analytics tracking');
