0e7a48b85894531f4dbb695416df19f5fa59ba61
riversyang <rivers.yang@gmail.com>
2013-09-06 12:06:21 +0800

    Merge flowbar to riversyang, disable ga object temporarily, correct style.less file.

--- web/js/app.js
+++ web/js/app.js
@@ -1,4 +1,6 @@
-define( ['Bootstrap', 'Fileupload', 'Favor'], function (Bootstrap, Fileupload, App) {
+define( ['Bootstrap', 'Fileupload', 'Favor', 'Flowbar'], function (Bootstrap, Fileupload, App, Flowbar) {
     var app = new App({});
     app.startup();
+    var flowbar = new Flowbar({});
+    flowbar.start();
 });
--- web/js/main.js
+++ web/js/main.js
@@ -37,6 +37,7 @@ require.config({
         Fileupload: '../../public/components/jquery-file-upload/js/jquery.fileupload',
         JST: '../../public/build/js/templates',
         Spa: 'backbone-spa',
+        Flowbar: 'flowbar',
         Favor: 'favor',
         Util: 'util',
         App: 'app'
