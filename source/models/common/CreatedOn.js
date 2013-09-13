dfb8c24c446631c153211f61939736f67b810688
henryleu <henryleu@126.com>
2013-09-13 21:01:13 +0800

    Add CreatedOn SchemaPlugin for CreatedOn type and time auto generating

---
+++ source/models/common/CreatedOn.js
@@ -0,0 +1,32 @@
+var SchemaPlugin = require('./SchemaPlugin');
+var idGen = require('../../../lib/id');
+var plugin = new SchemaPlugin({
+    name: 'createdOn',
+    prop: 'crtOn',
+    type: {
+        type: Date
+    },
+    use: function(schema, options){
+        //Add _id property
+        var path = {};
+        path[this.prop] = this.type;
+        schema.add(path);
+
+        //Add a save method's Preprocessor for id auto-generating
+        schema.pre('save', function (next) {
+            this.autoCreatedOn();
+            next()
+        });
+
+        //Add a instance method to ensure id: generate, set and return id
+        var prop = this.prop;
+        schema.method('autoCreatedOn', function () {
+            if(!this[prop]){
+                this[prop] = new Date();//TODO: use module to generate time
+            }
+            return this[prop];
+        })
+    }
+});
+
+module.exports = plugin;
