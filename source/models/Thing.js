e889d8c431e326d5d368cfe61b8f487222e11cfc
henryleu <henryleu@126.com>
2013-09-14 18:20:10 +0800

    Add Thing model in replace of Deal in the future

---
+++ source/models/Thing.js
@@ -0,0 +1,16 @@
+var mongoose = require('../../lib/mongoose');
+var DomainBuilder = require('./common/DomainBuilder');
+var schema = DomainBuilder
+    .i('Thing')
+    .withBasis()
+    .withCreatedOn()
+    .withCreatedBy()
+    .withUpdatedOn()
+    .withUpdatedBy()
+    .withProperties({
+        name: {type: String, required: true}
+    })
+    .build();
+
+module.exports.schema = schema;
+module.exports.model = mongoose.model(schema.name, schema);
