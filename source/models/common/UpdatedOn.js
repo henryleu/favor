1e854b29471d07bc4f68a5713f8299d49b76afba
henryleu <henryleu@126.com>
2013-09-14 18:15:49 +0800

    Add UpdatedOn schema plugin for createdOn auto generating and populating

---
+++ source/models/common/UpdatedOn.js
@@ -0,0 +1,30 @@
+var SchemaPlugin = require('./SchemaPlugin');
+var idGen = require('../../../lib/id');
+var plugin = new SchemaPlugin({
+    name: 'updatedOn',
+    prop: 'updOn',
+    type: {
+        type: Date
+    },
+    use: function(schema, options){
+        //Add the property to schema
+        var path = {};
+        path[this.prop] = this.type;
+        schema.add(path);
+
+        //Add a save method's Preprocessor for id auto-generating
+        schema.pre('save', function (next) {
+            this.autoUpdatedOn();
+            next()
+        });
+
+        //Add a instance method to ensure id: generate, set and return id
+        var prop = this.prop;
+        schema.method('autoUpdatedOn', function () {
+            this[prop] = new Date();//TODO: use module to generate time
+            return this[prop];
+        })
+    }
+});
+
+module.exports = plugin;
