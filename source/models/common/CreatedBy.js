ea4db75dc75c0e71ab650a6841bacd712ea5fc34
henryleu <henryleu@126.com>
2013-09-14 18:16:26 +0800

    Add CreatedBy schema plugin for createdBy auto populating

---
+++ source/models/common/CreatedBy.js
@@ -0,0 +1,21 @@
+var SchemaPlugin = require('./SchemaPlugin');
+var idGen = require('../../../lib/id');
+var plugin = new SchemaPlugin({
+    name: 'createdBy',
+    prop: 'crtBy',
+    type: {type: String, ref: 'User', default: null},
+    use: function(schema, options){
+        //Add the property to schema
+        var path = {};
+        path[this.prop] = this.type;
+        schema.add(path);
+
+        //Add a save method's Preprocessor for id auto-generating
+        schema.pre('save', function (next) {
+            //TODO: get and set current user id
+            next()
+        });
+    }
+});
+
+module.exports = plugin;
