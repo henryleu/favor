cb6f34e6134881c6440d94f45a77bdc8b4a33b38
henryleu <henryleu@126.com>
2013-09-15 18:54:52 +0800

    Add the integration test of UserService

---
+++ test/services/UserService.js
@@ -0,0 +1,47 @@
+var UserService = require('../../source/services/UserService');
+var mongoose = require('../../lib/mongoose');
+var logger = require('../../lib/logging').logger;
+var list = require('./../mocks/users');
+
+exports.setUp = function(done){
+    setTimeout(function(){done();}, 500);
+};
+exports.tearDown = function(done){
+    done()
+};
+exports.testCreateAnonymousUser = function(test){
+    UserService.create(function(err, user){
+        if(err){
+            logger.error('Fail to create anonymous user: '+err);
+        }
+        else{
+            logger.debug('Succeed to create anonymous user ' + JSON.stringify(user));
+            test.ok(true);
+        }
+    });
+    test.done();
+};
+exports.testLoadAnonymousUser = function(test){
+    UserService.create(function(err, user){
+        if(err){
+            logger.error('Fail to create anonymous user: '+err);
+        }
+        else{
+            logger.debug('Succeed to create anonymous user ' + JSON.stringify(user));
+            UserService.loadByUserToken(user.utoken, function(err, user){
+                if(err){
+                    logger.error('Fail to load anonymous user [' + user.utoken + ']: '+err);
+                }
+                else{
+                    if(user){
+                        logger.debug('Succeed to load anonymous user: ' + JSON.stringify(user));
+                    }
+                    else{
+                        logger.debug('Fail to load anonymous user [' + user.utoken + '], it doesn\'t exist ');
+                    }
+                }
+            });
+        }
+    });
+    test.done();
+};
