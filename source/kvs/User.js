66238b2c5232d61ba677b1c1aa12375a7c5c2422
henryleu <henryleu@126.com>
2013-09-15 18:50:11 +0800

    Add User redis dao object with save and load methods

---
+++ source/kvs/User.js
@@ -0,0 +1,47 @@
+var redis = require('../../lib/redis');
+var _ = require('underscore');
+var logger = require('../../lib/logging').logger;
+
+var userKey = function(utoken){
+    return 'utoken:' + utoken;
+};
+var userProps = ['id', 'lFlg', 'stt', 'utoken', 'username', 'email'];
+
+var User = {
+    load: function(utoken, callback){
+        var key = userKey(utoken);
+        redis.hgetall(key, function(err, result){
+            if(err){
+                logger.error('Fail to load anonymous user: ' + err);
+                callback(err, null);
+                return;
+            }
+
+            if(result){
+                callback(err, result);
+            }
+            else{
+                callback(null, null);
+            }
+        });
+    },
+    save: function(user, callback){
+        user = _.pick(Object(user), userProps);
+        var key = userKey(user.utoken);
+        redis.hmset(key, user, function(err, result){
+            if(err){
+                logger.error('Fail to create anonymous user: ' + err);
+                callback(err, null);
+                return;
+            }
+
+            if(result=='OK'){
+                callback(err, user);
+            }
+            else{
+                callback(new Error('Fail to create anonymous user'), null);
+            }
+        });
+    }
+};
+module.exports = User;
