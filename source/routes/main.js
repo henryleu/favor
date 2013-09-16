2bd7c624be7f6f2f9e9cb4cda18fabaeda24773e
henryleu <henryleu@126.com>
2013-09-16 11:50:03 +0800

    fix bug: change util.apply to util.extend

--- source/routes/main.js
+++ source/routes/main.js
@@ -1,75 +1,59 @@
 var logger = require('../../lib/logging').logger;
 var util = require('../../lib/util');
-var redis = require('../../lib/redis');
 var Deal = require('../models/Deal').model;
 var UserLikedDeal = require('../models/UserLikedDeal').model;
 var UserOwnedDeal = require('../models/UserOwnedDeal').model;
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
-/*
-        redis.set("test", "Hello World", function (err, reply) {
-            if(err){
-                logger.error(err);
-            }
-            logger.debug(reply);
-        });
-        redis.get("test", function (err, reply) {
-            if(err){
-                logger.error(err);
-            }
-            logger.debug(reply);
-        });
-        req.session.user = {name: 'henryleu', signinStatus: true};
-*/
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
     app.get('/home', function(req, res) {
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
     app.get('/share', function(req, res) {
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
     app.get('/find', function(req, res) {
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
     app.get('/catalog-newest', function(req, res) {
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
     app.get('/catalog-hottest', function(req, res) {
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
     app.get('/catalog-selfrun', function(req, res) {
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
 
     app.get('/profile', function(req, res) {
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
     app.get('/forum', function(req, res) {
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
     app.get('/about', function(req, res) {
         var input = {};
-        util.apply(input, req.asset || {});
+        util.extend(input, req.asset || {});
         res.render('index', input);
     });
     app.post('/deal', function(req, res) {
