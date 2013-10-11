1a28ac564a54eb847f47d5a145edcba317363ea3
henryleu <henryleu@126.com>
2013-10-11 21:55:02 +0800

    Mofiy things mock data

--- test/mocks/things.js
+++ test/mocks/things.js
@@ -1,5 +1,51 @@
 module.exports = [{
-    'name': 'iPhone 5s'
+    'name': 'Jawbone UP 2',
+    "image": "http://making-photos.b0.upaiyun.com/photos/2a7343fa66062c66c31586473124f009.jpg!normal",
+
+    //other image urls
+    images:[
+        'http://making-photos.b0.upaiyun.com/photos/2a7343fa66062c66c31586473124f009.jpg!normal',
+        'http://making-photos.b0.upaiyun.com/photos/2a7343fa66062c66c31586473124f009.jpg!normal',
+        'http://making-photos.b0.upaiyun.com/photos/2a7343fa66062c66c31586473124f009.jpg!normal'
+    ],
+
+    //the original url of the deal
+    "dUrl": "http://making-photos.b0.upaiyun.com/photos/2a7343fa66062c66c31586473124f009.jpg!normal",
+
+    //the original website of the deal: it is id of site which is maintain in other collections
+    "dSite": "",
+
+    //short description as a title for marketing
+    "sDesc": "Jawbone UP 2nd Generation - 你的生活小秘书",
+
+    //brief summary as a product name for product naming
+    "pSum": "Jawbone UP 2nd Generation",
+
+    //long description as detailed product information
+    "lDesc": "Jawbone UP 2nd Generation - 你的生活小秘书, Jawbone UP 2nd Generation - 你的生活小秘书"
 },{
-    'name': 'Jawbone UP 2'
+    'name': 'Herman Miller Aeron Chair',
+    "image": "http://making-photos.b0.upaiyun.com/photos/43a614d2be78892b64bb6282816b2787.jpg!review",
+
+    //other image urls
+    images:[
+        'http://making-photos.b0.upaiyun.com/photos/2a7343fa66062c66c31586473124f009.jpg!normal',
+        'http://making-photos.b0.upaiyun.com/photos/2a7343fa66062c66c31586473124f009.jpg!normal',
+        'http://making-photos.b0.upaiyun.com/photos/2a7343fa66062c66c31586473124f009.jpg!normal'
+    ],
+
+    //the original url of the deal
+    "dUrl": "http://making-photos.b0.upaiyun.com/photos/2a7343fa66062c66c31586473124f009.jpg!normal",
+
+    //the original website of the deal: it is id of site which is maintain in other collections
+    "dSite": "",
+
+    //short description as a title for marketing
+    "sDesc": "Herman Miller Aeron Chair - 人类有史以来最健康舒适的工作座椅",
+
+    //brief summary as a product name for product naming
+    "pSum": "Jawbone UP 2nd Generation",
+
+    //long description as detailed product information
+    "lDesc": "Jawbone UP 2nd Generation - 你的生活小秘书, Jawbone UP 2nd Generation - 你的生活小秘书"
 }];
