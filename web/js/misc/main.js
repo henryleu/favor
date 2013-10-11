a42c2ed6e4fbba3fcabb8c1bad76cf380cc989ff
henryleu <henryleu@126.com>
2013-10-11 21:51:08 +0800

    Add two modules: Upai and Util

--- web/js/misc/main.js
+++ web/js/misc/main.js
@@ -1,6 +1,8 @@
-define(['./Analytics'],
-function(Analytics) {
+define(['./Util', './Analytics', './Upai'],
+function(Util, Analytics, Upai) {
     var misc = {};
     misc.Analytics = Analytics;
+    //Util modify javascript native object
+    //Upai modify jQuery's export-object ($) as jQuery plugin
     return misc;
 });
