efc31d7c000fed966e996f507f7329d69ed93af1
henryleu <henryleu@126.com>
2013-10-23 19:00:25 +0800

    Add Kv class to manage user's meta info which is the history info user interacts with thing

--- source/kvs/UserMeta.js
+++ source/kvs/UserMeta.js
@@ -1,73 +1,131 @@
 var redis = require('../commons/redis');
-var _ = require('underscore');
 var logger = require('../commons/logging').logger;
 
-var userKey = function(utoken){
-    return 'utoken:' + utoken;
-};
-var userStarsKey = function(utoken){
-    return 'utoken:' + utoken + ':stars';
+var userStarsKey = function(uid){
+    return 'user:' + uid + ':stars';
 };
 
-var userProps = ['id', 'lFlg', 'stt', 'utoken', 'username', 'email', 'liked', 'owned'];
+var userLikesKey = function(uid){
+    return 'user:' + uid + ':likes';
+};
 
-var User = {
-    load: function(utoken, callback){
-        var key = userKey(utoken);
-        redis.hgetall(key, function(err, result){
+var UserMeta = {
+    countStars: function(uid, callback){
+        var key = userStarsKey(uid);
+        redis.hlen(key, function(err, result){
             if(err){
-                logger.error('Fail to load anonymous user: ' + err);
-                callback(err, null);
+                logger.error('Fail to get the length of stars of user ['+uid+']: ' + err);
+                callback(err, 0);
                 return;
             }
-
-            if(result){
-                callback(err, result);
-            }
-            else{
-                callback(null, null);
-            }
+            callback(null, result);
         });
     },
-    save: function(user, callback){
-        user = _.pick(Object(user), userProps);
-        var key = userKey(user.utoken);
-        redis.hmset(key, user, function(err, result){
+    star: function(uid, thingId, callback){
+        var key = userStarsKey(uid);
+        var now = new Date(); //TODO: uee time factory to get now time
+        var obj = {};
+        obj[thingId] = now.getTime();
+
+        redis.hmset(key, obj, function(err, result){
             if(err){
-                logger.error('Fail to create anonymous user: ' + err);
-                callback(err, null);
+                logger.error('Fail to let user ['+uid+'] star thing ['+thingId+']: ' + err);
+                callback(err, 0);
                 return;
             }
 
             if(result=='OK'){
-                callback(err, user);
+                callback(null, 1);
             }
             else{
-                callback(new Error('Fail to create anonymous user'), null);
+                callback(new Error('Fail to let user ['+uid+'] star thing [' + thingId + ']'), 0);
             }
         });
     },
-    saveProps: function(user, props, callback) {
-        var values = _.pick(Object(user), props);
-        var key = userKey(user.utoken);
-        redis.hmset(key, values, callback);
-    },
-    loadStars: function(utoken, callback){
-        var key = userStarsKey(utoken);
-        redis.hgetall(key, function(err, result){
+    unstar: function(uid, thingId, callback){
+        var key = userStarsKey(uid);
+        redis.hdel(key, thingId, function(err, result){
             if(err){
-                logger.error('Fail to load user\'s stars : ' + err);
-                callback(err, null);
+                logger.error('Fail to let user ['+uid+'] unstar thing ['+thingId+']: ' + err);
+                callback(err, 0);
                 return;
             }
 
-            if(result){
-                callback(err, result);
+            if(result==1){
+                callback(null, result);
             }
             else{
-                callback(null, {});//if not existent, return a blank object
+                callback(new Error('Fail to let user ['+uid+'] unstar thing ['+thingId+']: '), 0);
             }
         });
+    },
+    countLikes: function(uid, callback){
+        var key = userLikesKey(uid);
+        redis.hlen(key, function(err, result){
+            if(err){
+                logger.error('Fail to get the length of likes of user ['+uid+']: ' + err);
+                callback(err, 0);
+                return;
+            }
+            callback(null, result);
+        });
+    },
+    like: function(uid, thingId, callback){
+        var key = userLikesKey(uid);
+        var now = new Date(); //TODO: uee time factory to get now time
+        var obj = {};
+        obj[thingId] = now.getTime();
+
+        redis.hmset(key, obj, function(err, result){
+            if(err){
+                logger.error('Fail to let user ['+uid+'] like thing ['+thingId+']: ' + err);
+                callback(err, 0);
+                return;
+            }
+
+            if(result=='OK'){
+                callback(null, 1);
+            }
+            else{
+                callback(new Error('Fail to let user ['+uid+'] like thing [' + thingId + ']'), 0);
+            }
+        });
+    },
+    unlike: function(uid, thingId, callback){
+        var key = userLikesKey(uid);
+        redis.hdel(key, thingId, function(err, result){
+            if(err){
+                logger.error('Fail to let user ['+uid+'] unlike thing ['+thingId+']: ' + err);
+                callback(err, 0);
+                return;
+            }
+
+            if(result==1){
+                callback(null, result);
+            }
+            else{
+                callback(new Error('Fail to let user ['+uid+'] unlike thing ['+thingId+']: '), 0);
+            }
+        });
+    },
+    getMeta: function(uid, callback){
+        var starsKey = userStarsKey(uid);
+        var likesKey = userLikesKey(uid);
+        redis.multi()
+            .hgetall(starsKey)
+            .hgetall(likesKey)
+            .exec(function (err, replies) {
+                if (err) {
+                    logger.error('Fail to get meta info of user [' + uid + ']: ' + err);
+                    callback(err, null);
+                    return;
+                }
+
+                var meta = {};
+                meta.stars = replies[0] || {};
+                meta.likes = replies[1] || {};
+                callback(null, meta);
+            });
     }
 };
-module.exports = User;
+module.exports = UserMeta;
